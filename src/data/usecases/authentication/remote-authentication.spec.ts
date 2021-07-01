40ab7a6035b943427ae28aa9e93de8ff4640e1bc
Wisinewski <g_gomees@hotmail.com>
2021-06-30 23:50:45 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct url

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,26 @@
+import { mockAuthenticationParams } from './../../../domain/test/mock-authentication'
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '../../protocols/test/http/mock-http-client'
+
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('Remote Authentication', () => {
+  test('should call HttpPostClient with correct url', async () => {
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth(mockAuthenticationParams())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
