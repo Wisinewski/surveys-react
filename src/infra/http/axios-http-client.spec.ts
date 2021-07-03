e5c811750d94b54ea3634a96589af632f3efb0b0
Wisinewski <g_gomees@hotmail.com>
2021-07-03 17:26:12 -0300

    refactor: move axios mock to a separate file

---
+++ src/infra/http/axios-http-client.spec.ts
@@ -0,0 +1,35 @@
+import { mockAxios } from '@/infra/test/mock-axios'
+import { AxiosHttpClient } from './axios-http-client'
+import { mockPostRequest } from '@/data/protocols/test/http/mock-http-post'
+import axios from 'axios'
+
+jest.mock('axios')
+
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
+  return {
+    sut,
+    mockedAxios
+  }
+}
+
+describe('Axios Http Client', () => {
+  test('should call axios with correct values', async () => {
+    const request = mockPostRequest()
+    const { sut, mockedAxios } = makeSut()
+    await sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+  })
+
+  test('should return correct statusCode and body', () => {
+    const { sut, mockedAxios } = makeSut()
+    const promise = sut.post(mockPostRequest())
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
+  })
+})
