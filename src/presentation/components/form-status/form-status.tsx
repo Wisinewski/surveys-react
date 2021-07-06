6bc4afe8cd8007082679903490bda3adf9f7bf71
Wisinewski <g_gomees@hotmail.com>
2021-07-05 21:48:32 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -6,7 +6,7 @@ import React, { useContext } from 'react'
 const FormStatus: React.FC = () => {
   const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div data-testid='error-wrap' className={Styles.errorWrap}>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
       { isLoading && <Spinner className={Styles.spinner} /> }
       { errorMessage && <span className={Styles.error}>{errorMessage}</span> }
     </div>
