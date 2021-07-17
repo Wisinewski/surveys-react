ed7b741db53338646335862441aaaf919573a197
Wisinewski <g_gomees@hotmail.com>
2021-07-16 21:59:02 -0300

    feat: ensure Login calls Authentication only once

--- src/presentation/test/mock-authentication.ts
+++ src/presentation/test/mock-authentication.ts
@@ -6,8 +6,10 @@ import { Authentication } from '@/domain/usecases/authentication'
 export class AuthenticationSpy implements Authentication {
   account: AccountModel = mockAccountModel()
   params: AuthenticationParams
+  callsCount: number = 0
   async auth (authenticationParams: AuthenticationParams): Promise<AccountModel> {
     this.params = authenticationParams
+    this.callsCount++
     return Promise.resolve(this.account)
   }
 }
