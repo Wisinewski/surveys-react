371661d6e003193a816cfa82f30b99e770f1b43d
Wisinewski <g_gomees@hotmail.com>
2021-08-06 17:36:30 -0300

    feat: ensure EmailValidation returns an error if email is invalid

---
+++ src/presentation/validation/validators/email/email-validation.ts
@@ -0,0 +1,10 @@
+import { InvalidFieldError } from './../../errors/invalid-field-error';
+import { FieldValidation } from './../../protocols/field-validation';
+
+export class EmailValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
