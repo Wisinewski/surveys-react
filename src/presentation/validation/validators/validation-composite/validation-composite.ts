80439dc21f1f9b177234e11da63d5233dac33011
Wisinewski <g_gomees@hotmail.com>
2021-08-06 18:24:43 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/presentation/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,18 @@
+import { FieldValidation } from './../../protocols/field-validation'
+import { Validation } from './../../../protocols/validation'
+
+export class ValidationComposite implements Validation {
+  constructor (
+    private readonly validators: FieldValidation[]
+  ) {}
+
+  validate (fieldName: string, fieldValue: string): string {
+    const validators = this.validators.filter(validator => validator.field === fieldName)
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue)
+      if (error) {
+        return error.message
+      }
+    }
+  }
+}
