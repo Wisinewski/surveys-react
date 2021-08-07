425eade7e6e5ee1574b5376b29c42659a77afb7e
Wisinewski <g_gomees@hotmail.com>
2021-08-07 17:52:33 -0300

    test: ensure ValidationBuilder returns EmailValidation

--- src/presentation/validation/validators/builder/validation-builder.spec.ts
+++ src/presentation/validation/validators/builder/validation-builder.spec.ts
@@ -1,3 +1,4 @@
+import { EmailValidation } from './../email/email-validation'
 import { ValidationBuilder } from './validation-builder'
 import { RequiredFieldValidation } from './../required-field/required-field-validation'
 
@@ -5,5 +6,10 @@ describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
     const validations = ValidationBuilder.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('should return EmailValidation', () => {
+    const validations = ValidationBuilder.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
