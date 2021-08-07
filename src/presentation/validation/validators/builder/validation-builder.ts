7e1516606425d502a79b625aaaa1378c4a9d086b
Wisinewski <g_gomees@hotmail.com>
2021-08-07 17:52:26 -0300

    feat: ensure ValidationBuilder returns EmailValidation

--- src/presentation/validation/validators/builder/validation-builder.ts
+++ src/presentation/validation/validators/builder/validation-builder.ts
@@ -1,3 +1,4 @@
+import { EmailValidation } from './../email/email-validation'
 import { RequiredFieldValidation } from './../required-field/required-field-validation'
 import { FieldValidation } from './../../protocols/field-validation'
 
@@ -13,6 +14,11 @@ export class ValidationBuilder {
 
   required (): ValidationBuilder {
     this.validations.push(new RequiredFieldValidation(this.fieldName))
+    return this
+  }
+
+  email (): ValidationBuilder {
+    this.validations.push(new EmailValidation(this.fieldName))
     return this
   }
 
